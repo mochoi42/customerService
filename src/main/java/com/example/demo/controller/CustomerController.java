5a22b924b1cbbfcd2addb4620be656b45b78c53c
mochoi42 <principle950@naver.com>
2024-09-27 09:44:44 +0900

    1

---
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.dao.CustomerDAO;
+import com.example.demo.entity.Customer;
+
+@Controller
+public class CustomerController {
+	@Autowired
+	private CustomerDAO dao;
+	
+	@CrossOrigin(origins="*")
+	@ResponseBody
+	@GetMapping("/customer/list")
+	public List<Customer> list(){
+		return dao.findAll();
+	}
+	
+	@PostMapping("/customer/insert")
+	public String insertSubmit(Customer c) {
+		dao.save(c);
+		return "redirect:/customer/list";
+	}
+}
